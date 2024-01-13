40acd743d8f20f332f7facdec9722db31a3b779e
wrightkhlebisol <calebogundiya@gmail.com>
2024-01-13 00:13:21 +0000

    Validate parameters

--- cinema-tickets-javascript/src/pairtest/TicketService.js
+++ cinema-tickets-javascript/src/pairtest/TicketService.js
@@ -6,7 +6,25 @@ export default class TicketService {
    * Should only have private methods other than the one below.
    */
 
+
+  #validateParams(accountId, ticketTypes) {
+    if (accountId <= 0) {
+      throw new RangeError("Account ID should be greater than zero")
+    }
+
+    if (ticketTypes[0] === undefined || Object.keys(ticketTypes[0]).length === 0) {
+      throw new SyntaxError("Ticket type is required")
+    }
+    return true;
+  }
+
   purchaseTickets(accountId, ...ticketTypeRequests) {
     // throws InvalidPurchaseException
+    try {
+      this.#validateParams(accountId, ticketTypeRequests)
+    } catch (error) {
+      throw new InvalidPurchaseException(error.message)
+    }
+
   }
 }
