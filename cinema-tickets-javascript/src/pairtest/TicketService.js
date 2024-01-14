4f1368c8738b48ba823df43fb335d8baa196a1b0
wrightkhlebisol <calebogundiya@gmail.com>
2024-01-14 17:52:40 +0000

    Refactor validations

--- cinema-tickets-javascript/src/pairtest/TicketService.js
+++ cinema-tickets-javascript/src/pairtest/TicketService.js
@@ -1,4 +1,3 @@
-import TicketTypeRequest from "./lib/TicketTypeRequest.js";
 import InvalidPurchaseException from "./lib/InvalidPurchaseException.js";
 import TicketPaymentService from "../thirdparty/paymentgateway/TicketPaymentService.js";
 import SeatReservationService from "../thirdparty/seatbooking/SeatReservationService.js";
@@ -7,13 +6,10 @@ export default class TicketService {
   /**
    * Should only have private methods other than the one below.
    */
-  #childPrice = 10.0;
-  #adultPrice = 20.0;
-  #requestQuantityObj = {
-    adult: 0,
-    child: 0,
-    infant: 0
-  };
+  // #ticketPaymentService;
+  // #seatReservationService;
+  #childPrice = 1000;
+  #adultPrice = 2000;
 
   constructor(
     ticketPaymentService = new TicketPaymentService(),
@@ -31,15 +27,34 @@ export default class TicketService {
     return this.#adultPrice;
   }
 
+  purchaseTickets(accountId, ...ticketTypeRequests) {
+    // throws InvalidPurchaseException
+    try {
+      const ticketTypeObjs = ticketTypeRequests[0]
+      this.#validateParams(accountId, ticketTypeObjs);
+      const requestQuantityObject = this.#aggregateRequest(ticketTypeObjs)
+      this.#validateTicketType(requestQuantityObject);
+      this.#validateQuantity(requestQuantityObject);
+
+      let { quantity, amount, seats } = this.#getQuantityAmountAndSeats(requestQuantityObject);
+
+      // Make payment request first then seatreservation request
+      this.ticketPaymentService.makePayment(accountId, amount);
+      this.seatReservationService.reserveSeat(accountId, seats);
+      return { quantity, amount, seats };
+    } catch (error) {
+      throw new InvalidPurchaseException(error.message);
+    }
+  }
+
   #validateParams(accountId, ticketTypes) {
     if (accountId <= 0 || typeof accountId !== "number") {
       throw new RangeError("Account ID should be an integer greater than zero");
     }
-    if (!ticketTypes) {
+    if (!ticketTypes || ticketTypes.length === 0) {
       throw new Error("Ticket type is required");
     }
-
-    if (Array.isArray(ticketTypes[0])) {
+    if (Array.isArray(ticketTypes[0]) || ticketTypes[0] === null) {
       throw new TypeError("Ticket type should be a non empty object");
     }
   }
@@ -57,57 +72,49 @@ export default class TicketService {
     }
   }
 
-  #getQuantityAmountAndSeats(ticketRequestObj) {
-    let quantity
-
-    if (!Array.isArray(ticketRequestObj)) {
-      let ticketType = ticketRequestObj.getTicketType().toLowerCase()
-      this.#requestQuantityObj[ticketType] = ticketRequestObj.getNoOfTickets();
-      quantity = ticketRequestObj.getNoOfTickets();
-    } else {
-      this.#requestQuantityObj = this.#aggregateRequestQuantity(ticketRequestObj);
-
-      quantity = ticketRequestObj.reduce((total, eachTypeCount) =>
-        total + eachTypeCount.getNoOfTickets(), 0);
-    }
-
-    this.#validateTicketType(this.#requestQuantityObj);
-
-    if (quantity < 1 || quantity > 20) {
+  #validateQuantity(ticketRequestObj) {
+    if (ticketRequestObj.quantity < 1 || ticketRequestObj.quantity > 20) {
       throw new RangeError("Quantity should be between 1 and 20");
     }
-
-    let amount = (this.#requestQuantityObj['adult'] * this.#adultPrice) + (this.#requestQuantityObj['child'] * this.#childPrice);
-    let seats = this.#requestQuantityObj['adult'] + this.#requestQuantityObj['child'];
-    return { quantity, amount, seats };
   }
 
-  #aggregateRequestQuantity(ticketRequestObj) {
-    ticketRequestObj.forEach(request => {
-      const ticketType = request.getTicketType();
-      if (ticketType === 'ADULT') {
-        this.#requestQuantityObj.adult += request.getNoOfTickets();
-      } else if (ticketType === 'CHILD') {
-        this.#requestQuantityObj.child += request.getNoOfTickets();
-      } else {
-        this.#requestQuantityObj.infant += request.getNoOfTickets();
-      }
-    });
-    return this.#requestQuantityObj;
+  #getQuantityAmountAndSeats(requestQuantityObj) {
+    let amount = (requestQuantityObj['adult'] * this.#adultPrice) + (requestQuantityObj['child'] * this.#childPrice);
+    let seats = requestQuantityObj['adult'] + requestQuantityObj['child'];
+    return { quantity: requestQuantityObj.quantity, amount, seats };
   }
 
-  purchaseTickets(accountId, ...ticketTypeRequests) {
-    // throws InvalidPurchaseException
-    try {
-      this.#validateParams(accountId, ticketTypeRequests[0]);
-      let { quantity, amount, seats } = this.#getQuantityAmountAndSeats(ticketTypeRequests[0]);
+  #aggregateRequest(ticketRequestObj) {
+    const requestQuantityObj = {
+      adult: 0,
+      child: 0,
+      infant: 0,
+      quantity: 0
+    };
 
-      // Make payment request first then seatreservation request
-      this.ticketPaymentService.makePayment(accountId, amount);
-      this.seatReservationService.reserveSeat(accountId, seats);
-      return { quantity, amount, seats };
-    } catch (error) {
-      throw new InvalidPurchaseException(error.message);
+    if (!Array.isArray(ticketRequestObj)) {
+      let noOfTickets = ticketRequestObj.getNoOfTickets()
+      let ticketType = ticketRequestObj.getTicketType().toLowerCase()
+
+      requestQuantityObj[ticketType] = noOfTickets;
+      requestQuantityObj.quantity = noOfTickets;
+    } else {
+      ticketRequestObj.forEach(request => {
+
+        const ticketType = request.getTicketType();
+        requestQuantityObj.quantity += request.getNoOfTickets();
+
+        if (ticketType === 'ADULT') {
+          requestQuantityObj.adult += request.getNoOfTickets();
+        } else if (ticketType === 'CHILD') {
+          requestQuantityObj.child += request.getNoOfTickets();
+        } else {
+          requestQuantityObj.infant += request.getNoOfTickets();
+        }
+      });
+
     }
+
+    return requestQuantityObj;
   }
 }
